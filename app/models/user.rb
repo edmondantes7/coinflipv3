6a84b53443cbdc6a52b7a61d41347dd3076dcce4
Jinoo Baek <baek32jinoo@gmail.com>
2015-10-10 15:30:43 -0700

    Display coin flip results

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :coinflips
   #attr_accessible :balance
+
+  def last_coin_flip_result
+    something = self.coinflips.order(:created_at)
+    require 'pry'
+    binding.pry
+    return "tail" if self.coinflips.order(:created_at).last.coin_result
+    return "head"
+  end
 end

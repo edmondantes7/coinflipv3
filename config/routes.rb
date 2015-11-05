b48bd16de293d4d8ac319fbd3d3ed320080336dd
Jinoo Baek <baek32jinoo@gmail.com>
2015-11-04 19:43:13 -0800

    Add introductory text

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     resources :surveys, only: [:create, :new]
   end
   resources :data, only: [:index]
-  root 'users#new'
+  resources :introduction, only: [:index]
+  root 'introduction#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

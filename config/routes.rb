eca062757a428764f5a8382b84d5d68912dc0e06
Richard Dewey <rld2126@columbia.edu>
2015-11-17 18:55:09 -0800

    changes r3

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :data, only: [:index]
   resources :introduction, only: [:index]
   root 'introduction#index'
+  
+  get '/thanks', to: 'introduction#thanks'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
